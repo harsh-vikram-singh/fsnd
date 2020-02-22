dbf088b19574c708e8b56296d509d4fd577102fb
harsh-vikram-singh <singh.harshvikram@gmail.com>
2020-02-22 15:40:57 +0530

    added the enum class to generate options in Venue form, and used the custom validator

--- 01_fyyur/forms.py
+++ 01_fyyur/forms.py
@@ -38,6 +38,68 @@ class Genres(Enum):
     def generate_options(cls):
         return [(member.value, member.value) for name, member in cls.__members__.items()]
 
+
+class States(Enum):
+    AL = 'AL',
+    AK = 'AK',
+    AZ = 'AZ',
+    AR = 'AR',
+    CA = 'CA',
+    CO = 'CO',
+    CT = 'CT',
+    DE = 'DE',
+    DC = 'DC',
+    FL = 'FL',
+    GA = 'GA',
+    HI = 'HI',
+    ID = 'ID',
+    IL = 'IL',
+    IN = 'IN',
+    IA = 'IA',
+    KS = 'KS',
+    KY = 'KY',
+    LA = 'LA',
+    ME = 'ME',
+    MT = 'MT',
+    NE = 'NE',
+    NV = 'NV',
+    NH = 'NH',
+    NJ = 'NJ',
+    NM = 'NM',
+    NY = 'NY',
+    NC = 'NC',
+    ND = 'ND',
+    OH = 'OH',
+    OK = 'OK',
+    OR = 'OR',
+    MD = 'MD',
+    MA = 'MA',
+    MI = 'MI',
+    MN = 'MN',
+    MS = 'MS',
+    MO = 'MO',
+    PA = 'PA',
+    RI = 'RI',
+    SC = 'SC',
+    SD = 'SD',
+    TN = 'TN',
+    TX = 'TX',
+    UT = 'UT',
+    VT = 'VT',
+    VA = 'VA',
+    WA = 'WA',
+    WV = 'WV',
+    WI = 'WI',
+    WY = 'WY'
+
+    @classmethod
+    def list_all(cls):
+        return [member.value for name, member in cls.__members__.items()]
+
+    @classmethod
+    def generate_options(cls):
+        return [(member.value, member.value) for name, member in cls.__members__.items()]
+
 #########################################################################
 # custom validators
 
@@ -74,59 +136,7 @@ class VenueForm(Form):
     )
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=[
-            ('AL', 'AL'),
-            ('AK', 'AK'),
-            ('AZ', 'AZ'),
-            ('AR', 'AR'),
-            ('CA', 'CA'),
-            ('CO', 'CO'),
-            ('CT', 'CT'),
-            ('DE', 'DE'),
-            ('DC', 'DC'),
-            ('FL', 'FL'),
-            ('GA', 'GA'),
-            ('HI', 'HI'),
-            ('ID', 'ID'),
-            ('IL', 'IL'),
-            ('IN', 'IN'),
-            ('IA', 'IA'),
-            ('KS', 'KS'),
-            ('KY', 'KY'),
-            ('LA', 'LA'),
-            ('ME', 'ME'),
-            ('MT', 'MT'),
-            ('NE', 'NE'),
-            ('NV', 'NV'),
-            ('NH', 'NH'),
-            ('NJ', 'NJ'),
-            ('NM', 'NM'),
-            ('NY', 'NY'),
-            ('NC', 'NC'),
-            ('ND', 'ND'),
-            ('OH', 'OH'),
-            ('OK', 'OK'),
-            ('OR', 'OR'),
-            ('MD', 'MD'),
-            ('MA', 'MA'),
-            ('MI', 'MI'),
-            ('MN', 'MN'),
-            ('MS', 'MS'),
-            ('MO', 'MO'),
-            ('PA', 'PA'),
-            ('RI', 'RI'),
-            ('SC', 'SC'),
-            ('SD', 'SD'),
-            ('TN', 'TN'),
-            ('TX', 'TX'),
-            ('UT', 'UT'),
-            ('VT', 'VT'),
-            ('VA', 'VA'),
-            ('WA', 'WA'),
-            ('WV', 'WV'),
-            ('WI', 'WI'),
-            ('WY', 'WY'),
-        ]
+        choices=States.generate_options()
     )
     address = StringField(
         'address', validators=[DataRequired()]
@@ -139,7 +149,7 @@ class VenueForm(Form):
     )
     genres = SelectMultipleField(
         # TODO implement enum restriction
-        'genres', validators=[DataRequired()],
+        'genres', validators=[DataRequired(), validate_genres],
         choices=Genres.generate_options()
     )
     facebook_link = StringField(
@@ -172,59 +182,7 @@ class ArtistForm(Form):
     )
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=[
-            ('AL', 'AL'),
-            ('AK', 'AK'),
-            ('AZ', 'AZ'),
-            ('AR', 'AR'),
-            ('CA', 'CA'),
-            ('CO', 'CO'),
-            ('CT', 'CT'),
-            ('DE', 'DE'),
-            ('DC', 'DC'),
-            ('FL', 'FL'),
-            ('GA', 'GA'),
-            ('HI', 'HI'),
-            ('ID', 'ID'),
-            ('IL', 'IL'),
-            ('IN', 'IN'),
-            ('IA', 'IA'),
-            ('KS', 'KS'),
-            ('KY', 'KY'),
-            ('LA', 'LA'),
-            ('ME', 'ME'),
-            ('MT', 'MT'),
-            ('NE', 'NE'),
-            ('NV', 'NV'),
-            ('NH', 'NH'),
-            ('NJ', 'NJ'),
-            ('NM', 'NM'),
-            ('NY', 'NY'),
-            ('NC', 'NC'),
-            ('ND', 'ND'),
-            ('OH', 'OH'),
-            ('OK', 'OK'),
-            ('OR', 'OR'),
-            ('MD', 'MD'),
-            ('MA', 'MA'),
-            ('MI', 'MI'),
-            ('MN', 'MN'),
-            ('MS', 'MS'),
-            ('MO', 'MO'),
-            ('PA', 'PA'),
-            ('RI', 'RI'),
-            ('SC', 'SC'),
-            ('SD', 'SD'),
-            ('TN', 'TN'),
-            ('TX', 'TX'),
-            ('UT', 'UT'),
-            ('VT', 'VT'),
-            ('VA', 'VA'),
-            ('WA', 'WA'),
-            ('WV', 'WV'),
-            ('WI', 'WI'),
-            ('WY', 'WY'),
-        ]
+        choices=States.generate_options()
     )
     phone = StringField(
         # TODO implement validation logic for state
