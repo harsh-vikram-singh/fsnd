d5cf3738440b166bb950bf17a2b5c7e13b6286a2
harsh-vikram-singh <singh.harshvikram@gmail.com>
2020-03-09 15:02:35 +0530

    finalized the code for playing the game. added error handlers

--- 02_trivia_app/backend/flaskr/__init__.py
+++ 02_trivia_app/backend/flaskr/__init__.py
@@ -30,6 +30,8 @@ def create_app(test_config=None):
     @app.route('/categories')
     def get_all_categories():
         categories = Category.query.all()
+        if len(categories) == 0:
+            abort(404)
         formatted_categories = [category.format() for category in categories]
         # print(formatted_categories)
         return jsonify({
@@ -50,13 +52,23 @@ def create_app(test_config=None):
   '''
     @app.route('/questions')
     def get_questions():
-        print('endpoint: get_questions')
+        # print('endpoint: get_questions')
         page = request.args.get('page', 1, type=int)
-        questions = Question.query.paginate(page=page, per_page=10)
+        try:
+            questions = Question.query.paginate(page=page, per_page=10)
+            if questions.total == 0:
+                abort(404)
+        except:
+            abort(422)
         questions_list = questions.items
         formatted_questions = [question.format()
                                for question in questions_list]
-        categories = Category.query.all()
+        try:
+            categories = Category.query.all()
+            if len(categories) == 0:
+                abort(404)
+        except:
+            abort(422)
         formatted_categories = [category.format() for category in categories]
 
         return jsonify({
@@ -84,11 +96,22 @@ def create_app(test_config=None):
                 abort(404)
 
             question.delete()
-            questions = Question.query.paginate(page=1, per_page=10)
+            try:
+                questions = Question.query.paginate(page=1, per_page=10)
+                if questions.total == 0:
+                    abort(404)
+            except:
+                abort(422)
             questions_list = questions.items
+            ipdb.set_trace()
             formatted_questions = [question.format()
                                    for question in questions_list]
-            categories = Category.query.all()
+            try:
+                categories = Category.query.all()
+                if len(categories) == 0:
+                    abort(404)
+            except:
+                abort(400)
             formatted_categories = [category.format()
                                     for category in categories]
 
@@ -121,11 +144,19 @@ def create_app(test_config=None):
             abort(400)
 
         question_text = data.get('question')
+        if question_text is None:
+            abort(400)
         answer = data.get('answer')
+        if answer is None:
+            abort(400)
         difficulty = data.get('difficulty')
+        if difficulty is None:
+            abort(400)
         category = data.get('category')
+        if category is None:
+            abort(400)
 
-        print(question_text)
+        # print(question_text)
 
         question = Question(question=question_text,
                             answer=answer,
@@ -136,7 +167,12 @@ def create_app(test_config=None):
         except:
             abort(422)
 
-        questions = Question.query.paginate(page=1, per_page=10)
+        try:
+            questions = Question.query.paginate(page=1, per_page=10)
+            if questions.total == 0:
+                abort(404)
+        except:
+            abort(500)
         questions_list = questions.items
         formatted_questions = [question.format()
                                for question in questions_list]
@@ -163,12 +199,18 @@ def create_app(test_config=None):
   '''
     @app.route('/questions/search', methods=['POST'])
     def search_question():
-        data = request.get_json()
-        search_term = data.get('searchTerm')
+        try:
+            data = request.get_json()
+        except:
+            abort(400)
+        try:
+            search_term = data.get('searchTerm')
+        except:
+            abort(400)
         questions = Question.query.filter(
             Question.question.ilike(f'%{search_term}%')).all()
         formatted_questions = [question.format() for question in questions]
-        print(formatted_questions)
+        # print(formatted_questions)
         return jsonify({
             'success': True,
             'current_category': None,
@@ -185,8 +227,11 @@ def create_app(test_config=None):
   '''
     @app.route('/categories/<int:category_id>/questions')
     def category_questions(category_id):
-        questions = Question.query.filter(
-            Question.category == category_id).all()
+        try:
+            questions = Question.query.filter(
+                Question.category == category_id).all()
+        except:
+            abort(404)
         formatted_questions = [question.format() for question in questions]
         return jsonify({
             'success': True,
@@ -205,36 +250,91 @@ def create_app(test_config=None):
   one question at a time is displayed, the user is allowed to answer
   and shown whether they were correct or not. 
   '''
+    def choose_random_question(previous_questions, questions):
+        '''
+        helper function to choose a random question that has previously not been asked
+        '''
+        chosen_one = None
+        if len(previous_questions) == len(questions):
+            return chosen_one
+        while True:
+            index_selected = random.randrange(len(questions))
+            if questions[index_selected].id in previous_questions:
+                questions.pop(index_selected)
+                continue
+            chosen_one = questions[index_selected]
+            break
+        return chosen_one
+
     @app.route('/quizzes', methods=['POST'])
     def play_quiz():
-        print('endpoint: quizzes')
-        data = request.get_json()
-        print(data)
-        previous_question = data.get('previous_questions')
-        quiz_category = data.get('quiz_category')
+        try:
+            # print('endpoint: quizzes')
+            data = request.get_json()
+            # print(data)
+            previous_questions = data.get('previous_questions')
+            quiz_category = data.get('quiz_category')
 
-        # selecting the category for the questions to select from
-        all_categories = [c.id for c in Category.query.all()]
-        category = quiz_category.get('type')
-        if category == 'click':
-            category = all_categories
-        else:
-            category = [category]
+            # selecting the category for the questions to select from
+            all_categories = [c.id for c in Category.query.all()]
+            category = quiz_category.get('type')
+            if category == 'click':
+                category = all_categories
+            else:
+                category = [category]
 
-        # selecting question from the table based on the choice made by the user
-        # if len(category) == 1:
-        # ipdb.set_trace()
-        questions = Question.query.filter(
-            Question.category.in_(category)).all()
+            # selecting question from the table based on the choice made by the user
+            # if len(category) == 1:
+            # ipdb.set_trace()
+            questions = Question.query.filter(
+                Question.category.in_(category)).all()
 
-        return jsonify({
-            'success': True
-        })
+            chosen_one = choose_random_question(previous_questions, questions)
+            if chosen_one is not None:
+                chosen_one = chosen_one.format()
+
+            return jsonify({
+                'success': True,
+                'question': chosen_one
+            })
+        except:
+            abort(404)
 
     '''
   @TODO: 
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            'success': False,
+            'error': 404,
+            'message': 'Not Found'
+        }), 404
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return jsonify({
+            'success': False,
+            'error': 500,
+            'message': 'Server Error'
+        }), 500
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            'success': False,
+            'error': 400,
+            'message': 'Bad Request'
+        }), 400
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            'success': False,
+            'error': 422,
+            'message': 'Can not be processed. Please check your request again'
+        }), 422
 
     return app
