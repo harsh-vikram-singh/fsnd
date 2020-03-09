b60ec065c421a208ccda594e50354799eb2801dd
harsh-vikram-singh <singh.harshvikram@gmail.com>
2020-03-09 17:32:04 +0530

    added a unit test for testing the endpoint get_all_categories

--- 02_trivia_app/backend/test_flaskr.py
+++ 02_trivia_app/backend/test_flaskr.py
@@ -5,17 +5,22 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flaskr import create_app
 from models import setup_db, Question, Category
+import ipdb
+print('message at the top after imports')
 
 
 class TriviaTestCase(unittest.TestCase):
     """This class represents the trivia test case"""
 
     def setUp(self):
+        # ipdb.set_trace()
+        print('inside func: setup')
         """Define test variables and initialize app."""
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://postgres:postgres@{}/{}".format(
+            'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
 
         # binds the app to the current context
@@ -24,7 +29,7 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
@@ -33,6 +38,41 @@ class TriviaTestCase(unittest.TestCase):
     TODO
     Write at least one test for each test for successful operation and for expected errors.
     """
+    ######################################
+    # tests for endpoint: get_all_categories
+    ######################################
+
+    def test_get_all_categories(self):
+        ipdb.set_trace()
+        res = self.client().get('/categories')
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+
+    ######################################
+    # tests for endpoint: get_questions
+    ######################################
+
+    ######################################
+    # tests for endpoint: delete_questions
+    ######################################
+
+    ######################################
+    # tests for endpoint: add_question
+    ######################################
+
+    ######################################
+    # tests for endpoint: search_question
+    ######################################
+
+    ######################################
+    # tests for endpoint: category_questions
+    ######################################
+
+    ######################################
+    # tests for endpoint: play_quiz
+    ######################################
 
 
 # Make the tests conveniently executable
